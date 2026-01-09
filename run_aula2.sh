f406c473a98b3cd39b25bf50ad50c9ac78d471e2
Leison-dev <joseleison2024@gmail.com>
2026-01-09 20:45:43 -0300

    Delete run_aula2.sh

--- run_aula2.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# Compila e executa Aulas JAVA/aula2.java a partir da raiz do repositório
-ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
-SOURCE_DIR="$ROOT_DIR/Aulas JAVA"
-javac "$SOURCE_DIR/aula2.java"
-java -cp "$SOURCE_DIR" aula2
